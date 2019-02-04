f61ea253348df054148ebe4308bf4d231ba5fb7c
Noura <nourahnasser2@gmail.com>
2019-02-04 18:03:14 +0300

    Initinal Commit by Noura

---
+++ Twitter.py
@@ -0,0 +1,25 @@
+from searchtweets import load_credentials, ResultStream, gen_rule_payload, collect_results
+
+premium_search_args = load_credentials("./twitter_keys.yaml",
+                                       yaml_key="search_tweets_sandbox",
+                                       env_overwrite=False)
+# Testing with sandbox
+
+rule = gen_rule_payload(" حجاج العجمي lang:ar",
+                        from_date="2011-01-01",
+                        to_date="2018-12-31",
+                        results_per_call=100,
+                        )# testing with a sandbox account
+print(rule)
+
+'''tweets = collect_results(rule,
+                         max_results=100,
+                         result_stream_args=premium_search_args) # change this if you need to
+f = open("result.txt", "w+")
+
+[print(tweet.all_text, end='\n\n') for tweet in tweets[0:50]];
+
+
+        for each in self.cleanText:
+            f.write(each+'\n')
+        f.close()'''
